885ef45c041cc6b4b8ae11d516ce1b60c9db56e7
Oliver Ou <oliver.ou@sap.com>
2022-07-10 17:52:26 +0800

    changed table name; sample sql for users

--- postgres/init.sql
+++ postgres/init.sql
@@ -15,7 +15,7 @@ REVOKE ALL ON SCHEMA "93fe5029-1886-4b63-94ca-35c503a52eff" FROM public;
 
 
 -- create tenants table
-CREATE TABLE "fims"."tenants" (
+CREATE TABLE "fims"."a_tenants" (
     "id" uuid,
     "subdomain" text,
     "dsn" text,
@@ -24,7 +24,7 @@ CREATE TABLE "fims"."tenants" (
     PRIMARY KEY ("id")
 );
 -- init data for local tenant
-INSERT INTO "fims"."tenants" (
+INSERT INTO "fims"."a_tenants" (
     "id",
     "subdomain",
     "dsn",
@@ -39,4 +39,4 @@ INSERT INTO "fims"."tenants" (
 );
 
 -- change owner
-ALTER TABLE "fims"."tenants" OWNER TO "fims";
+ALTER TABLE "fims"."a_tenants" OWNER TO "fims";
---
+++ scripts/new_app_user.sql
@@ -0,0 +1,8 @@
+INSERT INTO "a_users" ("id", "traits", "created_at", "updated_at")
+VALUES ('4a923c5f-ab84-4267-b6de-1f9b841ec270', '{name: {last: "欧", first: "朝阳"}, email: "o@fims.com"}', now(), now());
+
+INSERT INTO "a_users" ("id", "traits", "created_at", "updated_at")
+VALUES ('254fb592-070e-4bfa-b96b-c1264fc36942', '{name: {last: "林", first: "俊杰"}, email: "l@fims.com"}', now(), now());
+
+INSERT INTO "a_users" ("id", "traits", "created_at", "updated_at")
+VALUES ('90eb2fd2-5f25-4108-bac4-ae203e8933ee', '{name: {last: "风", first: "格伦"}, email: "f@fims.com"}', now(), now());
