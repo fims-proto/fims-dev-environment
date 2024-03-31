05fdcde6c4ab9ed2b4896a912e48895b07908df9
Linfeng Ou <linfeng.ou@gmail.com>
2024-03-31 20:03:39 +0800

    Upversion Kratos and Oathkeeper

--- postgres/init.sql
+++ postgres/init.sql
@@ -6,3 +6,7 @@ CREATE SCHEMA "fims-db" AUTHORIZATION "fims-db";
 
 -- revoke authorization for tenant admin
 REVOKE ALL ON SCHEMA "fims-db" FROM public;
+
+-- create extensions which could be used in Kratos migration
+CREATE EXTENSION IF NOT EXISTS "pg_trgm";
+CREATE EXTENSION IF NOT EXISTS "btree_gin";
